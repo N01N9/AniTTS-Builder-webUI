9d2c0358ca0fa5035c0491a2ecf3c0fcc00b7d36
N01N9 <060311sdh@gmail.com>
2024-09-23 20:17:26 +0900

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,35 +1,35 @@
 @echo off
 chcp 65001 > NUL
 
-:: 관리자 권한 확인
+pushd %~dp0
+
 openfiles >nul 2>&1
 if %ERRORLEVEL% neq 0 (
-    echo This script requires administrator privileges. Restarting as Administrator...
-    powershell -Command "Start-Process '%~f0' -Verb RunAs"
-    exit /b
-)
-
-:: 가상환경 경로 설정 (main.py와 같은 디렉터리에 venv 폴더가 있을 경우)
-set VENV_DIR=%~dp0venv
-set PYTHON_CMD=%VENV_DIR%\Scripts\python.exe
-
-:: main.py 경로 설정 (main.py가 이 배치 파일과 동일한 디렉터리에 있다고 가정)
-set MAIN_SCRIPT=%~dp0main.py
-
-:: 가상환경 활성화
-echo Activating virtual environment...
-call "%VENV_DIR%\Scripts\activate.bat"
-if %ERRORLEVEL% neq 0 (
-    echo [ERROR] Failed to activate the virtual environment.
+    echo This script requires administrator privileges.
+    echo Please run this script as Administrator.
     pause
+    popd
     exit /b 1
 )
 
-echo Current batch file directory: %~dp0
+echo Activating Conda environment...
+call conda activate AniTTS_Builder_webUI
+if %ERRORLEVEL% neq 0 (
+    echo Failed to activate the Conda environment.
+    pause
+    popd
+    exit /b 1
+)
 
-:: main.py 실행
-echo Running main.py as Administrator...
-"%PYTHON_CMD%" "%MAIN_SCRIPT%"
+echo Running main.py with administrator privileges...
+python main.py
 
-:: 종료 전 대기
+if %ERRORLEVEL% neq 0 (
+    echo  main.py failed to run.
+    pause
+    popd
+    exit /b %ERRORLEVEL%
+)
+
+popd
 pause
